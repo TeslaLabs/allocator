25b67bac505d86b9c566acb695b5dea0ed18685d
Kevin Ndung'u <kevgathuku@gmail.com>
2016-08-10 16:25:56 +0300

    Add docopt docstring

--- allocator.py
+++ allocator.py
@@ -1,63 +1,43 @@
 #!/usr/bin/env python
+"""                         Allocator
 
+This program handles room allocation system for office and living spaces
+in one of Andela's facilities called Amity.
+
+Usage:
+  allocator.py create_rooms (living|office) <room_name>...
+  allocator.py add_person (fellow|staff) <name>... [--accomodation=n]
+  allocator.py reallocate_person ((fellow|staff) <person_identifier> <new_room_name>)...
+  allocator.py print_allocations [--output-allocations=allocations.txt]
+  allocator.py print_unallocated [--output-unallocated=unallocated.txt]
+  allocator.py print_room <room_name>
+  allocator.py load_people <people_input_file>
+  allocator.py load_state <sqlite_database>
+  allocator.py (-i | --interactive)
+  allocator.py (-h | --help)
+  allocator.py --version
+
+Options:
+  -h, --help                      Show this screen and exit.
+  -i, --interactive               Interactive Mode.
+  -a, --accomodation=(Y|N)        Wants accomodation [default: N].
+  -oa, --output-allocations FILE  Specify output file [default: ./allocations.txt]
+  -ou, --output-unallocated FILE  Specify output file [default: ./unallocated.txt]
+  --version                       Show version.
+
+"""
 from __future__ import print_function
+
+__version__ = '0.1.0'
 
 import random
 import sys
 
+from docopt import docopt
+
 from models import Fellow, LivingSpace, Office, Staff
-
-people_file = 'seeds/people.txt'
-
-
-def allocate(people, rooms):
-    """c
-    Parameters
-    ----------
-    people : list of Person-derived instances
-        This is a list of instances that are derived from the Person class.
-        They can be either of type Fellow or Staff
-    rooms : list of Room-derived instances
-        This is a list of instances that are derived from the Room class.
-        They can be either of type LivingSpace or Office
-    """
-    if not people:
-        print("No people available to allocate rooms to")
-        sys.exit(0)
-    available = True  # Variable to save whether rooms are still available
-    for person in people:
-        room = random.choice(rooms)
-        room.add_person(person)
 
 
 if __name__ == '__main__':
-    # Populate people
-    unallocated = []
-    spaces = []
-    with open(people_file) as people:
-        for line in people:
-            data = line.rstrip().split()
-            name = '{} {}'.format(data[0], data[1])
-            role = data[2]
-            if role == 'FELLOW':
-                accomodation = data[3]
-                fellow = Fellow(name, accomodation)
-                unallocated.append(fellow)
-                print(fellow.name, fellow.living_space_choice)
-            else:
-                staff = Staff(name)
-                unallocated.append(fellow)
-                print(staff.name)
-
-    # Populate Office and Living Spaces
-    with open(rooms_file) as all_spaces:
-        for space in all_spaces:
-            space_data = space.rstrip().split()
-            space_name = space_data[0]
-            office_type = space_data[1]
-            if office_type == 'OFFICE':
-                office = Office(space_name)
-                spaces.append(office)
-            elif office_type == 'LIVING':
-                living = LivingSpace(name)
-                spaces.append(living)
+    args = docopt(__doc__, version=__version__)
+    print(args)
