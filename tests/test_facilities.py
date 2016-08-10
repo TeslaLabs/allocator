819a32d53173fcc6f8d40ee069c6e299285fa2c5
Kevin Ndung'u <kevgathuku@gmail.com>
2016-08-10 19:46:01 +0300

    Test Room creation functionality

--- tests/test_facilities.py
+++ tests/test_facilities.py
@@ -7,16 +7,25 @@ class FacilityTest(unittest.TestCase):
     """ Tests for the Facility Class """
 
     def setUp(self):
+        self.test_facility = models.Facility('Test-Amity')
         pass
 
     def tearDown(self):
         pass
 
     def test_create_facility(self):
+        self.assertEqual('Test-Amity', self.test_facility.name)
+        self.assertEqual(0, len(self.test_facility.people))
+        self.assertEqual(0, len(self.test_facility.rooms))
         pass
 
-    def test_create_rooms(self):
-        pass
+    def test_create_living_space(self):
+        self.test_facility.create_rooms('living_space', ['one', 'two'])
+        self.assertEqual(2, len(self.test_facility.rooms))
+
+    def test_create_office(self):
+        self.test_facility.create_rooms('office', ['one', 'two'])
+        self.assertEqual(2, len(self.test_facility.rooms))
 
     def test_add_person(self):
         pass
